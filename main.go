a482023d9ffcf6373797ec20c4c5a72a054bdead
RahulVPol <rahul.pol@cimpress.com>
2020-12-19 10:49:38 +0530

    lecture-1-commit-3 Added /goodbye handler handled post request @ path '/'

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -39,7 +41,42 @@ func main(){
 	// 3: run curl localhost:9090
 		// now you will get Hello World with time stamp at server
 		// in fact for any path you will get the same output
-	h1 := func(http.ResponseWriter, *http.Request){
+	h1 := func(rw http.ResponseWriter, r *http.Request){
+		// the handler function comes with two paramters
+		// http.Request -> represent http request. It has properties like Method, URL, Body
+		// http.ResponseWriter -> its an interface to construct http response
+
+		// Note that http.ResponseWriter is an interface, so there must be a
+		// concrete implementation of this interface.
+		// And it is response struct which you will find in http package
+		// just follow the trail of http.ListenAndServe(), it has a Server
+		// Server has a connection(conn), conn has readRequest and initializes
+		// response, and this response implements methods of http.ResponseWriter
+		// which are Header(), Write() and WriteHeader()
+
+		// try running curl -X POST -d "Rahul" localhost:9090
+		// this creates post request for / path with data "Rahul"
+		// to read data received from body
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil{
+			// This is base method to reply to a host
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Oops"))
+
+			// however http also provides an Error method that will wrap
+			// your response writer, message and an error code
+			http.Error(rw,"Oops", http.StatusBadRequest)
+			
+			return
+		}
+
+		log.Printf("Data %s\n",d)
+
+		// cool now we can read the data from our API
+		// lets try to write data back to our client
+		fmt.Fprintf(rw, "Hello %s",d)
+		
 		log.Println("Hello World")
 	}
 
@@ -47,6 +84,15 @@ func main(){
 	// so, in simple words we are saying map h1 for all paths 	
 	http.HandleFunc("/",h1) 
 
+	
+	h2 := func(http.ResponseWriter, *http.Request){
+		log.Println("Good Bye")
+	}
+
+	// This is just another example of HandleFunc with different pattern(path)
+	// Notice that when you request server with goodbye path it won't execute
+	// both Hello World and Good Bye 
+	http.HandleFunc("/goodbye", h2)
 	
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
